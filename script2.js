c30fc59b322b6f4299594ae5637151bf6a1194a5
golemoon <62132553+golemoon@users.noreply.github.com>
2023-09-22 11:51:14 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,13 @@
+"use strict"
+
+function saluer(){
+    console.log("Bonjour bienvenue")
+}
+saluer();
+
+let pseudo="Ralftou";
+function saluerQqun(prenom){
+    console.log("Bonjour"+ prenom);
+}
+saluerQqun("Bob");
+saluerQqun(pseudo);
